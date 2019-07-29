14468e0871bd552d2da4acec560562f0c46d716f
Abdul Rehman <arehman1031@gmail.com>
2019-07-29 07:10:20 +0500

    Packages on parlor page

--- app/Parlor.php
+++ app/Parlor.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Parlor extends Model
 {
-    //
+	public function packages()
+	{
+		return $this->hasMany('\App\Package');
+	}
 }
--- resources/views/site/parlor.blade.php
+++ resources/views/site/parlor.blade.php
@@ -11,6 +11,29 @@
 			<hr/>
 			<p> {{ $parlor->desc }}</p>
 		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<h2 class="slider-title">Packages</h2>
+			</div>
+			@foreach($parlor->packages as $key => $package)
+			<div class="col-md-4">
+				<div class="news">
+					<div class="entry-date">
+						<div class="date">{{ $package->created_at->format('d') }}</div>
+						<div class="monthyear">{{ $package->created_at->format('M.Y') }}</div>
+						<br>
+						<a href="{{route('book', $package->id)}}" class="button">Book</a>
+						<br>
+						<br>
+						<a href="{{route('add_like', $package->id)}}"><i class="fa fa-thumbs-up col-brown"></i></a>
+					</div>
+					<div class="entry-detail">
+						<div class="entry-summary"> <a href="{{ route('package_detail', $package->id) }}" class="more-icon"><img src="{{ Storage::url($package->image) }}"></a></div>
+					</div>
+				</div>
+			</div>
+			@endforeach
+		</div> <!-- .row -->
 	</div>
 </div>
 @endsection
