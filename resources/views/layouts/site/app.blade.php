c041b4b346c112531ecaaa5685ed6733126ef144
Abdul Rehman <arehman1031@gmail.com>
2019-07-31 13:18:35 +0500

    menu selection fixed for home

--- resources/views/layouts/site/app.blade.php
+++ resources/views/layouts/site/app.blade.php
@@ -39,7 +39,7 @@
 					<div class="main-navigation">
 						<button type="button" class="menu-toggle"><i class="fa fa-bars"></i></button>
 						<ul class="menu">
-							<li class="menu-item {{ Request::is('index*') ? 'current-menu-item' : '' }}"><a href="/">Home</a></li>
+							<li class="menu-item {{ Request::is('/*') ? 'current-menu-item' : '' }}"><a href="/">Home</a></li>
 							<li class="menu-item {{ Request::is('about*') ? 'current-menu-item' : '' }}"><a href="{{ route('about') }}">About</a></li>
 							<li class="menu-item {{ Request::is('services*') ? 'current-menu-item' : '' }}"><a href="{{ route('services') }}">Services</a></li>
 							<li class="menu-item {{ Request::is('gallery*') ? 'current-menu-item' : '' }}"><a href="{{ route('gallery') }}">Gallery</a></li>
