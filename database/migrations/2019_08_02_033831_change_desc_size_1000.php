271465e1f0d88e36dc1123846bc4dda2556b2109
Abdul Rehman <arehman1031@gmail.com>
2019-08-02 04:12:13 +0500

    slider fixed, description 1000, search form

---
+++ database/migrations/2019_08_02_033831_change_desc_size_1000.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeDescSize1000 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+		Schema::table('categories', function (Blueprint $table) {
+		    $table->string('desc', 1000)->nullable()->change();
+		});
+
+		Schema::table('parlors', function (Blueprint $table) {
+		    $table->string('desc', 1000)->nullable()->change();
+		});
+
+		Schema::table('packages', function (Blueprint $table) {
+		    $table->string('desc', 1000)->nullable()->change();
+		});
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
---
+++ resources/views/layouts/site/slider.blade.php
@@ -0,0 +1,28 @@
+<div class="hero hero-slider">
+	<ul class="slides">
+		<li data-bg-image="{{asset('dummy/slideimg1.jpg')}}">
+			<div class="container">
+				<h3 class="slider-subtitle">What are u looking for?</h3>
+				<h2 class="slider-title">Professional</h2>
+				<p>This is the platform you are finding where you can book any service in your desired parlor or salon....!</p>
+				<a href="#" class="button large">Read more</a>
+			</div>
+		</li>
+		<li data-bg-image="{{asset('dummy/slideimg4.jpg')}}">
+			<div class="container">
+				<h3 class="slider-subtitle">What are u looking for?</h3>
+				<h2 class="slider-title">Professional</h2>
+				<p>This is the platform you are finding where you can book any service in your desired parlor or salon....!</p>
+				<a href="#" class="button large">Read more</a>
+			</div>
+		</li>
+		<li data-bg-image="{{asset('dummy/slideimg3.jpg')}}">
+			<div class="container">
+				<h3 class="slider-subtitle">What are u looking for?</h3>
+				<h2 class="slider-title">Professional</h2>
+				<p>This is the platform you are finding where you can book any service in your desired parlor or salon....!</p>
+				<a href="#" class="button large">Read more</a>
+			</div>
+		</li>
+	</ul>
+</div>
